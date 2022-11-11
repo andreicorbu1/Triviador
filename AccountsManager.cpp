8b81d8b3342085dab114b2fcf58d5b66bd5df7b1
andreicorbu1 <andrei.corbu@student.unitbv.ro>
2022-11-11 22:02:24 +0200

    Added functionality to Search for a user based on his username and Get the User object by his username

--- AccountsManager.cpp
+++ AccountsManager.cpp
@@ -8,6 +8,21 @@ void AccountManager::AddUser(const User& user)
 	}
 }
 
+bool AccountManager::SearchUser(const std::string& username) const
+{
+	// True if successful, false otherwise
+	return m_accounts.contains(username);
+}
+
+User AccountManager::GetUser(const std::string& username) const
+{
+	if(SearchUser(username))
+	{
+		return m_accounts.at(username);
+	}
+	return {};
+}
+
 bool AccountManager::ValidateCredentials(const User& user) const
 {
 	if (std::regex_match(user.GetPassword(), std::regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,25}$")))
@@ -19,6 +34,7 @@ bool AccountManager::ValidateCredentials(const User& user) const
 
 std::ostream& operator<<(std::ostream& os, const AccountManager& manager)
 {
+	//Temporary implementation, waiting for class User << operator override
 	for(auto it : manager.m_accounts)
 	{
 		os << it.second.GetUsername() << "\n" << it.second.GetPassword() << "\n" << it.second.GetGamesPlayed() << "\n" << it.second.GetLevel() << "\n";
