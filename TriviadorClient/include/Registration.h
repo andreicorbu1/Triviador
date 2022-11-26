98106fe68c8c2c0171c5e0b749d347434bf32894
TibiTiberiu35 <tiberiuioan35@gmail.com>
2022-11-26 13:16:05 +0200

    Add validation for username and password

--- TriviadorClient/include/Registration.h
+++ TriviadorClient/include/Registration.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QWidget>
+#include <regex>
 #include <cpr/cpr.h>
 
 #include "ui_Registration.h"
@@ -16,6 +17,11 @@ public:
 private slots:
     void on_logInButton_clicked() const;
     void on_signUpButton_clicked() const;
+
+private:
+    bool IsValidUsername(const std::string& username) const;
+    bool IsValidPassword(const std::string& password) const;
+    bool ValidateCredentials(const std::string& username, const std::string& password) const;
 
 private:
     Ui::RegistrationClass ui;
--- TriviadorClient/src/Registration.cpp
+++ TriviadorClient/src/Registration.cpp
@@ -12,21 +12,56 @@ Registration::~Registration()
 
 void Registration::on_logInButton_clicked() const
 {
+    std::string username = ui.usernameInput->text().toStdString();
+    std::string password = ui.passwordInput->text().toStdString();
+
+    ValidateCredentials(username, password);
+    
     qDebug() << "Log in button clicked";
     
 }
 
 void Registration::on_signUpButton_clicked() const
 {
+    std::string username = ui.usernameInput->text().toStdString();
+    std::string password = ui.passwordInput->text().toStdString();
+    
+    if (!ValidateCredentials(username, password)) return;
+
     qDebug() << "Sign up button clicked";
 }
 
-//bool Registration::IsValidUsername(const std::string& username)
-//{
-//    return username.length() >= 3 && username.length() <= 10;
-//}
-//
-//bool Registration::IsValidPassword(const std::string& password)
-//{
-//    return password.length() >= 8 && password.length() <= 20;
-//}
+bool Registration::IsValidUsername(const std::string& username) const
+{
+    return username.length() >= 3 && username.length() <= 10;
+}
+
+bool Registration::IsValidPassword(const std::string& password) const
+{
+    std::regex passwordRegex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,25}$");
+    return std::regex_match(password, passwordRegex);
+}
+
+bool Registration::ValidateCredentials(const std::string& username, const std::string& password) const
+{
+    bool isValidUsername = IsValidUsername(username);
+    bool isValidPassword = IsValidPassword(password);
+    
+    if (isValidUsername)
+    {
+        ui.usernameInput->setStyleSheet("border: 2px solid #FCF321");
+    }
+    else {
+        ui.usernameInput->setStyleSheet("border: 2px solid #9B1212");
+    }
+
+    if (isValidPassword)
+    {
+        ui.passwordInput->setStyleSheet("border: 2px solid #FCF321");
+    }
+    else {
+        ui.passwordInput->setStyleSheet("border: 2px solid #9B1212");
+    }
+
+    return isValidUsername && isValidPassword;
+}
