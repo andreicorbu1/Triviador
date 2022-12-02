3ef8cc68b02abac6a120f28dd0d41ea8fad945d5
Bostan Adrian <bostan.adrian02@gmail.com>
2022-12-02 16:34:40 +0200

    Created CRUD for Questions Database

--- TriviadorServer/TriviadorServer/src/QuestionManager.cpp
+++ TriviadorServer/TriviadorServer/src/QuestionManager.cpp
@@ -16,6 +16,68 @@ void QuestionManager::AddQuestion(const NumericalAnswerQuestion& question)
 	m_storage.insert(question);
 }
 
+void QuestionManager::RemoveMultipleAnswerQuestion(int id)
+{
+	m_storage.remove<MultipleAnswerQuestion>(id);
+}
+
+void QuestionManager::RemoveNumericalAnswerQuestion(int id)
+{
+	m_storage.remove<NumericalAnswerQuestion>(id);
+}
+
+void QuestionManager::UpdateQuestion(const MultipleAnswerQuestion& multipleAnswerQuestion)
+{
+	try
+	{
+		m_storage.get<MultipleAnswerQuestion>(multipleAnswerQuestion.GetId());
+		m_storage.replace(multipleAnswerQuestion);
+	}
+	catch(std::system_error e)
+	{
+		std::cout << "ID: " << multipleAnswerQuestion.GetId() << " " << e.what() << "\n";
+	}
+}
+
+void QuestionManager::UpdateQuestion(const NumericalAnswerQuestion& numericalAnswerQuestion)
+{
+	try
+	{
+		m_storage.get <NumericalAnswerQuestion>(numericalAnswerQuestion.GetId());
+		m_storage.replace(numericalAnswerQuestion);
+	}
+	catch (std::system_error e)
+	{
+		std::cout << "ID: " << numericalAnswerQuestion.GetId() << " " << e.what()<<"\n";
+	}
+}
+
+const MultipleAnswerQuestion& QuestionManager::GetMultipleAnswerQuestion(int id)
+{
+	try
+	{
+		return m_storage.get<MultipleAnswerQuestion>(id);
+	}
+	catch (std::system_error e)
+	{
+		std::cout <<"ID: " <<id<<" "<< e.what() << "\n";
+		return MultipleAnswerQuestion();
+	}
+}
+
+const NumericalAnswerQuestion& QuestionManager::GetNumericalAnswerQuestion(int id)
+{
+	try
+	{
+		return m_storage.get<NumericalAnswerQuestion>(id);
+	}
+	catch (std::system_error e)
+	{
+		std::cout << "ID: " << id << " " << e.what() << "\n";
+		return NumericalAnswerQuestion();
+	}
+}
+
 void QuestionManager::PopulateStorage()
 {
 	if (m_storage.count<MultipleAnswerQuestion>() == 0 && m_storage.count<NumericalAnswerQuestion>() == 0)
