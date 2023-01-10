4564c35faba4392223b84932c372d23254ab4a38
Ciortea Cristian Gabirel <cristian.ciortea@student.unitbv.ro>
2023-01-10 21:48:59 +0200

    Small changes to playerhistory class

--- TriviadorServer/TriviadorServer/src/PlayerHistory.cpp
+++ TriviadorServer/TriviadorServer/src/PlayerHistory.cpp
@@ -1,9 +1,10 @@
 #include "PlayerHistory.h"
 
-PlayerHistory::PlayerHistory(const Player& player)
+PlayerHistory::PlayerHistory(const Player& player, int32_t gameID)
 	:m_username(player.GetName())
 	,m_score(player.GetScore())
 	,m_rank(player.GetRank())
+	,m_ID(gameID)
 {
 	//empty
 }
@@ -23,6 +24,11 @@ void PlayerHistory::SetRank(size_t rank)
 	m_rank = rank;
 }
 
+void PlayerHistory::SetID(int id)
+{
+	m_ID = id;
+}
+
 std::string PlayerHistory::GetUsername() const
 {
 	return m_username;
@@ -36,4 +42,9 @@ int PlayerHistory::GetScore() const
 size_t PlayerHistory::GetRank() const
 {
 	return m_rank;
+}
+
+int PlayerHistory::GetID() const
+{
+	return m_ID;
 }
