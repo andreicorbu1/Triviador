bb9066721c7d6393ccfb36d0361d180c8aa3762e
Andrei Corbu <corbu.andrei@yahoo.com>
2023-01-14 00:28:18 +0200

    Made DLL common with server.

--- DLL/User.cpp
+++ DLL/User.cpp
@@ -1,12 +1,44 @@
 #include "User.h"
 
-User::User(const std::string& username) : m_username(username)
+User::User(const std::string& username, const std::string& password) :
+	m_username(username),
+	m_password(password)
 {
+	m_level = 0;
+	m_gamesPlayed = 0;
+	m_points = 0;
+	m_id = -1;
 }
+
+User::User(User&& user) noexcept
+{
+	*this = std::move(user);
+}
+
 
 void User::SetUsername(const std::string& username)
 {
 	m_username = username;
+}
+
+void User::SetPassword(const std::string& password)
+{
+	m_password = password;
+}
+
+void User::SetLevel(int level)
+{
+	m_level = level;
+}
+
+void User::SetGamesPlayed(int gamesPlayed)
+{
+	m_gamesPlayed = gamesPlayed;
+}
+
+void User::SetPoints(int points)
+{
+	m_points = points;
 }
 
 void User::SetId(int id)
@@ -14,9 +46,41 @@ void User::SetId(int id)
 	m_id = id;
 }
 
-std::string User::GetUsername() const
+void User::AddPoints(int points)
+{
+	m_points += points;
+}
+
+void User::UpdateLevel(int matchPoints)
+{
+	AddPoints(matchPoints);
+	m_level = m_level + static_cast<int>(m_points / 1000);
+	m_points = m_points % 1000;
+}
+
+const std::string& User::GetUsername() const
 {
 	return m_username;
+}
+
+const std::string& User::GetPassword() const
+{
+	return m_password;
+}
+
+int User::GetLevel() const
+{
+	return m_level;
+}
+
+int User::GetGamesPlayed() const
+{
+	return m_gamesPlayed;
+}
+
+int User::GetPoints() const
+{
+	return m_points;
 }
 
 int User::GetId() const
@@ -24,3 +88,17 @@ int User::GetId() const
 	return m_id;
 }
 
+User& User::operator=(User&& user) noexcept
+{
+	if (this != &user)
+	{
+
+		m_username = std::exchange(user.m_username, std::string());
+		m_password = std::exchange(user.m_password, std::string());
+		m_level = std::exchange(user.m_level, 0);
+		m_gamesPlayed = std::exchange(user.m_gamesPlayed, 0);
+		m_points = std::exchange(user.m_points, 0);
+		m_id = std::exchange(user.m_id, 0);
+	}
+	return *this;
+}
--- DLL/User.h
+++ DLL/User.h
@@ -4,21 +4,50 @@
 #else
 #define DLL_API __declspec(dllimport)
 #endif
-#include <string>
+#include<string>
+#include<iostream>
+
 class DLL_API User
 {
 public:
 	User() = default;
-	User(const std::string& username);
+	User(const std::string& username, const std::string& password = "");
+	User(const User& user) = default;
+	User(User&& user) noexcept;
+	~User() = default;
 
-	//Setters
+	//Setters:
 	void SetUsername(const std::string& username);
+	void SetPassword(const std::string& password);
+	void SetLevel(int level);
+	void SetGamesPlayed(int gamesPlayed);
+	void SetPoints(int points);
 	void SetId(int id);
 
-	//Getters
-	std::string GetUsername() const;
+	//Getters:
+	const std::string& GetUsername() const;
+	const std::string& GetPassword() const;
+	int GetLevel() const;
+	int GetGamesPlayed() const;
+	int GetPoints() const;
 	int GetId() const;
+
+	//Operators:
+	User& operator=(const User& user) = default;
+	User& operator=(User&& user) noexcept;
+
+	// Other methods:
+	void UpdateLevel(int matchPoints);
+
 private:
-	int m_id = -1;
+	void AddPoints(int points);
+
+private:
+	int m_id;
 	std::string m_username;
+	std::string m_password;
+	int m_level;
+	int m_gamesPlayed;
+	int m_points;
 };
+
